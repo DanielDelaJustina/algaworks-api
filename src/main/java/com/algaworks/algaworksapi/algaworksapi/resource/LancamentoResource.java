5ec3e8ff22bbe2524e9fb454a7a14214cf6b2d23
danieldelajustina <dd.justina@hotmail.com>
2019-02-06 16:10:30 -0200

    Criação de recurso para novos lançamentos

--- src/main/java/com/algaworks/algaworksapi/algaworksapi/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algaworksapi/algaworksapi/resource/LancamentoResource.java
@@ -1,14 +1,16 @@
 package com.algaworks.algaworksapi.algaworksapi.resource;
 
+import com.algaworks.algaworksapi.algaworksapi.event.RecursoCriadoEvent;
 import com.algaworks.algaworksapi.algaworksapi.model.Lancamento;
 import com.algaworks.algaworksapi.algaworksapi.repository.LancamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/lancamentos")
@@ -17,6 +19,9 @@ public class LancamentoResource {
 
     @Autowired
     LancamentoRepository lancamentoRepository;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
     @GetMapping
     public ResponseEntity<?> buscarLancamentos() {
@@ -30,5 +35,15 @@ public class LancamentoResource {
 
         Lancamento lancamento = lancamentoRepository.findOne(id);
         return lancamento == null ? ResponseEntity.notFound().build() : ResponseEntity.ok().body(lancamento);
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> criarLancamento(@RequestBody @Valid Lancamento lancamento, HttpServletResponse response) {
+
+        Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getId()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
     }
 }
