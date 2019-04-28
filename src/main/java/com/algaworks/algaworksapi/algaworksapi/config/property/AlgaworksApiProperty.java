e9b97490baf9653de2118b268a985cd292aff466
danieldelajustina <dd.justina@hotmail.com>
2019-04-28 15:16:11 -0300

    adicionando porta do front

--- src/main/java/com/algaworks/algaworksapi/algaworksapi/config/property/AlgaworksApiProperty.java
+++ src/main/java/com/algaworks/algaworksapi/algaworksapi/config/property/AlgaworksApiProperty.java
@@ -9,7 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("algaworks")
 public class AlgaworksApiProperty {
 
-    private String origemPermitida = "http://localhost:8000";
+    private String origemPermitida = "http://localhost:4200";
     private final Seguranca seguranca = new Seguranca();
 
     @Getter
